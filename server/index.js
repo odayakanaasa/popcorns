ea09c19fe1593c33b6df0b13d62cb14948de8035
Yub <lewisyubu@gmail.com>
2017-08-20 14:00:28 -0500

    init

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import { Nuxt, Builder } from 'nuxt'
+import bodyParser from 'body-parser'
+import { graphqlExpress } from 'apollo-server-express'
+
+const app = express()
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+
+// const schema = 
+
+app.set('port', port)
+
+// Import and Set Nuxt.js options
+let config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+// Init Nuxt.js
+const nuxt = new Nuxt(config)
+
+// Build only in dev mode
+if (config.dev) {
+  const builder = new Builder(nuxt)
+  builder.build()
+}
+
+// Give nuxt middleware to express
+app.use(nuxt.render)
+
+// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+
+// Listen the server
+app.listen(port, host)
+console.log('Server listening on ' + host + ':' + port) // eslint-disable-line no-console
